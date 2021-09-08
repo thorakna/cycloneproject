1a9548a1161c8f7b18f12b0a1b3ca82296f1cc7a
Umut Türk <74135564+umttrk19@users.noreply.github.com>
2021-09-08 15:52:10 +0300

    login

--- Backend/app.js
+++ Backend/app.js
@@ -3,40 +3,42 @@ const app = express();
 const bodyParser = require('body-parser')
 const User = require('./models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET="apğsdoğasdc#[${>#$Q^)=+343sdş**hjcdsd65f4dfklpaerts!!'£££23"
 app.get('/', (req, res) => {
     res.send("test")
 })
 app.use(bodyParser.json())
 //app.use(bp.urlencoded({ extended: true }))
 
-app.post('/giris', async (req, res, next) => {
-    const data = req.body
-    console.log(data.mail);
-    console.log(data.password);
-    //query
+app.post('/login', async (req, res, next) => {
+    const { username, password } = req.body
+    const user = await User.findOne({ username }).lean();
+    if (!user) {
+        return res.status(400).json({ status: 'fail', message: 'Invalid username or password' })
+    }
+    if (await bcrypt.compare(password, user.hashedPassword)) {
+        const token = jwt.sign({
+            id: user._id,
+            username: user.username
 
-    // if (data.mail === "umtrk1" && data.password === "1234") {
-    // res.status(200).json({
-    // status: 'success',
-    // isCorrect: true
-    // })
-    // } else {
-    // res.status(400).json({ status: 'fail', isCorrect: false })
-    // }
-
+        },JWT_SECRET)
+        return res.status(200).json({ status: 'success' ,data:token})
+    }
+    return res.status(400).json({ status: 'fail', message: 'Invalid username or password' })
 })
 app.post('/register', async (req, res, next) => {
 
     const { username, password, mail, imageUrl } = req.body
 
-    
+
     if (!username || typeof username !== 'string') {
-        return res.status(404).json({ status: 'error', message: 'Invalid username' })
+        return res.status(400).json({ status: 'error', message: 'Invalid username' })
     }
     if (!password || typeof password !== 'string') {
-        return res.status(404).json({ status: 'error', message: 'Invalid password' })
-    } if (password.length < 6) {
-        return res.status(404).json({ status: 'error', message: 'Too small password.Should be greater than 6 character' })
+        return res.status(400).json({ status: 'error', message: 'Invalid password' })
+    } if (password.length < 4) {
+        return res.status(400).json({ status: 'error', message: 'Too small password.Should be greater than 6 character' })
     }
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -49,6 +51,7 @@ app.post('/register', async (req, res, next) => {
             imageUrl
         }).then(() => {
             console.log("User has been registered succesfully");
+            //res.setHeader('Content-Type', 'text/plain');
             return res.status(200).json({ status: 'success' })
         })
 
@@ -60,8 +63,9 @@ app.post('/register', async (req, res, next) => {
         if (error.code === 11000 && Object.keys(error.keyPattern)[0] === 'username') {
             return res.status(404).json({ status: 'error', duplicate: 'username' })
         }
+        return res.status(404).json({ status: 'error' })
     }
     //hashing the passwords
-    return res.status(404).json({ status: 'error' })
+
 })
 module.exports = app;
