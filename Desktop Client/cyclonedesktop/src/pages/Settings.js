e3b2bec2ea4ca88ae65b0ec2629f742bd19c157c
Thorakna <arbedeci@gmail.com>
2021-10-03 00:39:34 +0300

    Settings page styling

--- Desktop Client/cyclonedesktop/src/pages/Settings.js
+++ Desktop Client/cyclonedesktop/src/pages/Settings.js
@@ -1,19 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import '../style/parallax-star.css';
-import '../style/Home.css';
-import { IoExit, IoSend, IoSettings, IoPeople, IoSearch } from "react-icons/io5";
+import '../style/Settings.css';
+import { IoPerson, IoInformation } from "react-icons/io5";
 
 
 export default function Settings() {
   const history = useHistory();
+  const [fullname, setFullname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   
   return (
     <>
-        <div className="ChatContainer">
-          <div className="ChatHeader">
-            <div className="UserDetails">
-              Burası settings sayfası!
+        <div className="PageContainer">
+          <div className="PageHeader">
+            <h4>Settings</h4>
+            <aside className="descGray">Edit your profile settings</aside>
+          </div>
+          <div className="Page" style={{overflowY:"auto"}}>
+            <div className="settingsPlace">
+              <div className="setfield">
+                Fullname
+                <input type="text" id="FullnameInput" value={fullname} autoComplete="off" onChange={(e)=>{setFullname(e.target.value);}}></input>
+              </div>
+              <div className="setfield" style={{animationDelay: 0.5+"s"}}>
+                Username
+                <input type="text" id="UsernameInput" value={username} autoComplete="off" onChange={(e)=>{setUsername(e.target.value);}}></input>
+              </div>
+              <div className="setfield" style={{animationDelay: 1+"s"}}>
+                Email Address
+                <input type="text" id="EmailInput" value={email} autoComplete="off" onChange={(e)=>{setEmail(e.target.value);}}></input>
+              </div>
             </div>
           </div>
         </div>
